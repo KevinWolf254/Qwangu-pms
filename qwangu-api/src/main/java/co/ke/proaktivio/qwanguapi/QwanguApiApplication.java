40f9b829b621673b7e7fa0e8248aedc34d5ba2cc
KevinWolf254 <mbugua.kevinkanyi@gmail.com>
2022-04-29 16:15:41 +0300

    initial commit

---
+++ qwangu-api/src/main/java/co/ke/proaktivio/qwanguapi/QwanguApiApplication.java
@@ -0,0 +1,13 @@
+package co.ke.proaktivio.qwanguapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwanguApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QwanguApiApplication.class, args);
+	}
+
+}
---
+++ qwangu-api/src/test/java/co/ke/proaktivio/qwanguapi/QwanguApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ke.proaktivio.qwanguapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwanguApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
