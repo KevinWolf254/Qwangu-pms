de091da38ac9d8da1e5ce7a2aec093bb4ff2102c
KevinWolf254 <mbugua.kevinkanyi@gmail.com>
2022-06-19 11:32:08 +0300

    clean up

--- qwangu-api/src/main/java/co/ke/proaktivio/qwanguapi/repositories/ApartmentRepository.java
+++ qwangu-api/src/main/java/co/ke/proaktivio/qwanguapi/repositories/ApartmentRepository.java
@@ -1,6 +1,7 @@
 package co.ke.proaktivio.qwanguapi.repositories;
 
 import co.ke.proaktivio.qwanguapi.models.Apartment;
+import co.ke.proaktivio.qwanguapi.repositories.custom.CustomApartmentRepository;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
 
