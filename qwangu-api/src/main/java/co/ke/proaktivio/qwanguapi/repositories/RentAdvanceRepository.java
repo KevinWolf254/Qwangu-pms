3952a6e700b002c42cb4239ff044c555d4958750
KevinWolf254 <mbugua.kevinkanyi@gmail.com>
2023-03-19 19:10:22 +0300

    Closes #25

--- qwangu-api/src/main/java/co/ke/proaktivio/qwanguapi/repositories/RentAdvanceRepository.java
+++
@@ -1,7 +0,0 @@
-package co.ke.proaktivio.qwanguapi.repositories;
-
-import co.ke.proaktivio.qwanguapi.models.RentAdvance;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface RentAdvanceRepository extends ReactiveMongoRepository<RentAdvance, String> {
-}
